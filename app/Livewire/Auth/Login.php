4ed1c7ab93aa152d8d50bb35ed820837767d055d
MuhammadWildanf <wildanf.daftar@gmail.com>
2025-12-25 20:12:17 +0700

    [add] add function login form

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,10 +2,57 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Auth;
+use Jantinnerezo\LivewireAlert\Facades\LivewireAlert;
 use Livewire\Component;
 
 class Login extends Component
 {
+
+
+    public $email, $password, $remember = false;
+
+    public function login()
+    {
+        $this->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        try {
+            if (Auth::attempt([
+                'email' => $this->email,
+                'password' => $this->password,
+            ], $this->remember)) {
+                $user = Auth::user();
+
+                /*
+                activity()
+                    ->causedBy($user)
+                    ->withProperties(['ip' => request()->ip()])
+                    ->event('login')
+                    ->log("telah melakukan login");
+                */
+
+                LivewireAlert::title('Selamat datang di dashboard Employee Management System')
+                    ->success()
+                    ->show();
+                return redirect()->route('dashboard.index');
+            }
+
+            LivewireAlert::title('Email atau password salah!')
+                ->error()
+                ->show();
+            return back();
+        } catch (\Throwable $th) {
+            report($th);
+            LivewireAlert::title('Terjadi kesalahan, silakan coba lagi!')
+                ->error()
+                ->show();
+            return back();
+        }
+    }
+
     public function render()
     {
         return view('livewire.auth.login')->layout('components.layouts.guest');
--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -32,6 +32,8 @@
 
     <!-- App js -->
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 
 </html>
